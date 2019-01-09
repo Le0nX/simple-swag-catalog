4ad38e191a44c5afc187036e4641e684af46203e
Le0nX <cloudmindone@gmail.com>
2019-01-09 16:05:44 +0300

    feature: added custom cell

---
+++ swag_catalog/swag_catalog/Controller/CategoriesVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swag_catalog
+//
+//  Created by Denis Nefedov on 09/01/2019.
+//  Copyright © 2019 X. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ swag_catalog/swag_catalog/Views/CategoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryCell.swift
+//  swag_catalog
+//
+//  Created by Denis Nefedov on 09/01/2019.
+//  Copyright © 2019 X. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

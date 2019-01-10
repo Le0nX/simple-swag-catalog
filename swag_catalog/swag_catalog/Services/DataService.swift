d67212361214b9873b626f65b83fb45dc684b981
Le0nX <cloudmindone@gmail.com>
2019-01-10 04:21:57 +0300

    feature: added collection view & some data about store assets

--- swag_catalog/swag_catalog/Services/DataService.swift
+++ swag_catalog/swag_catalog/Services/DataService.swift
@@ -20,6 +20,34 @@ class DataService {
         Category(title: "DIGITAL", imageName: "digital.png")
     ]
     
+    private let hats = [
+        Product(title: "Damn hat", price: "$12", imageName: "hat01.png" ),
+        Product(title: "Swaggy hat", price: "$100", imageName: "hat02.png" ),
+        Product(title: "Lucky hat", price: "$12", imageName: "hat03.png" ),
+        Product(title: "Killer hat", price: "$12", imageName: "hat04.png" )
+    ]
+    
+    private let hoodies = [
+        Product(title: "Damn hat", price: "$12", imageName: "hat01.png" ),
+        Product(title: "Swaggy hat", price: "$100", imageName: "hat02.png" ),
+        Product(title: "Lucky hat", price: "$12", imageName: "hat03.png" ),
+        Product(title: "Killer hat", price: "$12", imageName: "hat04.png" )
+    ]
+    
+    private let shirts = [
+        Product(title: "Damn hat", price: "$12", imageName: "hat01.png" ),
+        Product(title: "Swaggy hat", price: "$100", imageName: "hat02.png" ),
+        Product(title: "Lucky hat", price: "$12", imageName: "hat03.png" ),
+        Product(title: "Killer hat", price: "$12", imageName: "hat04.png" )
+    ]
+    
+    private let digital = [
+        Product(title: "Damn hat", price: "$12", imageName: "hat01.png" ),
+        Product(title: "Swaggy hat", price: "$100", imageName: "hat02.png" ),
+        Product(title: "Lucky hat", price: "$12", imageName: "hat03.png" ),
+        Product(title: "Killer hat", price: "$12", imageName: "hat04.png" )
+    ]
+    
     /* give categories from example Server */
     func getCategories() -> [Category] {
         return categories
---
+++ swag_catalog/swag_catalog/Views/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  swag_catalog
+//
+//  Created by Denis Nefedov on 10/01/2019.
+//  Copyright © 2019 X. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
